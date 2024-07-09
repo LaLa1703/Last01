d7b589fd4b390ba90e611ff792f71f6c72c3565b
LaLa1703 <ilakostenko402@gmail.com>
2024-07-10 01:07:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+completed_homeworks = 12
+spent_hours = 1.5
+course_name = "Python"
+time_per_homework = spent_hours / completed_homeworks
+print('Курс:', course_name, 'всего задач:', completed_homeworks, 'затрачено часов:', spent_hours, 'среднее время выполнения', time_per_homework, 'часа.')
+
