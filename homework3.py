7449bb9579405b64c57f6b76a08e320fc801b94d
LaLa1703 <ilakostenko402@gmail.com>
2024-07-12 17:36:54 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -2,7 +2,8 @@ name = "Ilya"
 print(name, type(name))
 age = 21
 print(age, type(age))
-age = age + 1
-print(age, type(age))
+new_age = 22
+age = age + new_age
+print(new_age, type(new_age))
 is_student = True
 print(is_student, type(is_student))
