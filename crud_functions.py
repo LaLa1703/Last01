0f765d25f52e9276e8eea9d5baeb2f4b91e94750
LaLa1703 <ilakostenko402@gmail.com>
2025-01-14 15:19:16 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -12,6 +12,16 @@ def initiate_db():
         price INTEGER NOT NULL
     )
     ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users ( 
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL DEFAULT 1000
+    )
+    ''')
     connection.commit()
     connection.close()
 
@@ -24,6 +34,20 @@ def get_all_products():
     connection.close()
     return products
 
+def add_user(username, email, age):
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?,?,?,?)", (username, email, age, 1000))
+    connection.commit()
+    connection.close()
+
+def is_included(username):
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Users WHERE username = ?", (username,))
+    user = cursor.fetchone()
+    connection.close()
+    return user is not None
 def populate_products():
     conn = sqlite3.connect('not_telegram.db')
     cursor = conn.cursor()
