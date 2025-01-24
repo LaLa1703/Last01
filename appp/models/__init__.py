01f8cc53c5af39cc0924dda0da215b54d23477d7
LaLa1703 <ilakostenko402@gmail.com>
2025-01-24 17:06:04 +0300

    Add files via upload

---
+++ appp/models/__init__.py
@@ -0,0 +1,11 @@
+from appp.backend.db import *
+from appp.models.user import User
+from appp.models.task import Task
+
+
+Base.metadata.create_all(bind=engine)
+
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(User.__table__).compile(engine))
+print(CreateTable(Task.__table__).compile(engine))
